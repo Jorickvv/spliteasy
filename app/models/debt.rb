b239aefd9360cddacaba5def61bab34129d3c7b3
Deniss Mazalevskis <Demmaza13@gmail.com>
2026-09-10 14:09:50 +0100

    Add settle up for debts

--- app/models/debt.rb
+++ app/models/debt.rb
@@ -1,12 +1,17 @@
 class Debt < ApplicationRecord
   belongs_to :group
   belongs_to :expense
+  belongs_to :debtor, class_name: "User", foreign_key: :debtor_id
+  belongs_to :creditor, class_name: "User", foreign_key: :creditor_id
 
-  belongs_to :debtor,
-             class_name: "User",
-             foreign_key: :debtor_id
+  before_validation :set_default_settled, on: :create
 
-  belongs_to :creditor,
-             class_name: "User",
-             foreign_key: :creditor_id
+  scope :active, -> { where(settled: false) }
+  scope :settled, -> { where(settled: true) }
+
+  private
+
+  def set_default_settled
+    self.settled = false if settled.nil?
+  end
 end
